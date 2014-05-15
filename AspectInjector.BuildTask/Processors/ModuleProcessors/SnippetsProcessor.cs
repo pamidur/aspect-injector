a78f9a7cfef4353821bec734720906798256c111
Alexander Guly <alexander.guly@outlook.com>
2014-05-15 23:54:56 +0300

    thinking about snippets injection

--- AspectInjector.BuildTask/Processors/ModuleProcessors/SnippetsProcessor.cs
+++ AspectInjector.BuildTask/Processors/ModuleProcessors/SnippetsProcessor.cs
@@ -1,10 +1,25 @@
 ﻿using AspectInjector.BuildTask.Contracts;
 using Mono.Cecil;
+using System.IO;
 
 namespace AspectInjector.BuildTask.Processors.ModuleProcessors
 {
     class SnippetsProcessor : IModuleProcessor
     {
+        public SnippetsProcessor()
+        {            
+            string assemblyFile = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            
+            var assembly = AssemblyDefinition.ReadAssembly(assemblyFile,
+                new ReaderParameters
+                {
+                    ReadingMode = Mono.Cecil.ReadingMode.Deferred,
+                });
+
+
+
+        }
+
         private static readonly string _namespace = "__$_aspect_injector_namespaces";
 
         public void ProcessModule(Mono.Cecil.ModuleDefinition module)
