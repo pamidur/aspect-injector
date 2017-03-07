371da690800f7e1fbdca51c717f24aabe766148c
Oleksandr Hulyi <aguly@outlook.com>
2017-03-08 00:05:56 +0200

    moved to vs2017, added Package project, removed release tool

--- src/AspectInjector.BuildTask/Contracts/IAspectInjector.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AspectInjector.BuildTask.Contracts
-{
-    public interface IAspectInjector<in TContext>
-        where TContext : class, IInjectionContext
-    {
-        void Inject(TContext context);
-    }
-}
