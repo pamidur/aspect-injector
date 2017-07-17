71aea4372fece4e6983c8c9bca45f9e7f800d920
Alexander Guly <alexander.guly@outlook.com>
2017-07-17 14:54:11 +0300

    moved main projects to new .csproj and net45

--- src/AspectInjector.Core.Mixin/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("AspectInjector.Core.InterfaceProxy")]
-[assembly: AssemblyProduct("AspectInjector")]
-[assembly: AssemblyCopyright("Copyright © AspectInjectorTeam 2016")]
-[assembly: AssemblyVersion("9999.0.0")]
-[assembly: AssemblyFileVersion("9999.0.0")]
-[assembly: AssemblyInformationalVersion("9999.0.0")]
