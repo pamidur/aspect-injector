7293af98fc49aa10e768b21bde04838917ccab5e
Oleksandr Hulyi <agulyj@live.com>
2019-06-16 21:38:23 +0300

    added some tests

---
+++ tests/AspectInjector.Tests.Runtime/General/PropagationControlTests.cs
@@ -0,0 +1,64 @@
+﻿using AspectInjector.Broker;
+using System;
+using Xunit;
+
+namespace AspectInjector.Tests.Runtime.General
+{
+    public class PropagationControlTests
+    {
+        private readonly TestClass _testTarget;
+
+        public PropagationControlTests()
+        {
+            _testTarget = new TestClass();
+        }
+
+        [Fact]
+        public void Propagation_Options_Correctly_Processed()
+        {
+            _testTarget.Method();
+            _testTarget.Event += (s, e) => { };
+            _testTarget.Property = "";
+        }
+
+        [EventTrigger]
+        [PropertyTrigger]
+        [MethodTrigger]
+        public class TestClass
+        {
+            public event EventHandler Event;
+            public string Property { get; set; }
+            public void Method() { }
+        }
+
+        [Injection(typeof(SuccessAspect), Propagation = PropagateTo.Events)]
+        public class EventTrigger : Attribute { }
+
+        [Injection(typeof(SuccessAspect), Propagation = PropagateTo.Properties)]
+        public class PropertyTrigger : Attribute { }
+
+        [Injection(typeof(SuccessAspect), Propagation = PropagateTo.Methods)]
+        public class MethodTrigger : Attribute { }
+
+
+        [Aspect(Scope.Global)]        
+        public class SuccessAspect
+        {
+            [Advice(Kind.Before, Targets =Target.Method | Target.EventAdd | Target.EventRemove | Target.Getter | Target.Setter )]
+            public void Success([Argument(Source.Name)] string name, [Argument(Source.Triggers)] Attribute[] triggers)
+            {
+                switch (name)
+                {
+                    case nameof(TestClass.Event):
+                        Assert.True(triggers.Length == 1 && triggers[0] is EventTrigger);break;
+                    case nameof(TestClass.Property):
+                        Assert.True(triggers.Length == 1 && triggers[0] is PropertyTrigger); break;
+                    case nameof(TestClass.Method):
+                        Assert.True(triggers.Length == 1 && triggers[0] is MethodTrigger); break;
+                    default:
+                        Assert.True(false);break;
+                }                
+            }
+        }
+    }
+}
