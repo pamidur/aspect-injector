380d2040810da5698ff73c69867b450d55e0a9af
Oleksandr Hulyi <ohulyi@outlook.com>
2020-01-18 22:14:21 +0100

    fixed #97

--- tests/AspectInjector.Tests.Runtime/Issues/0097.cs
+++ tests/AspectInjector.Tests.Runtime/Issues/0097.cs
@@ -1,39 +1,41 @@
-﻿//using AspectInjector.Broker;
-//using AspectInjector.Tests.Assets;
-//using System;
-//using Xunit;
+﻿using AspectInjector.Broker;
+using AspectInjector.Tests.Assets;
+using AspectInjector.Tests.RuntimeAssets.CrossAssemblyHelpers;
+using System;
+using Xunit;
 
-//namespace AspectInjector.Tests.Runtime.Issues
-//{
-//    public class Issue_0097
-//    {
-//        [Fact]
-//        public void Fixed()
-//        {
-//            new Target().Value = 12;
-//        }
+namespace AspectInjector.Tests.Runtime.Issues
+{
+    public class Issue_0097
+    {
+        [Fact]
+        public void Fixed()
+        {
+            new Target().Value = 12;
+        }
 
-//        private class TargetBase<T>
-//        {
-//            [TestAspect]
-//            public string Text { get; set; }
-//        }
+        private class SuperTargetBase<T> : TestBaseClass<T>
+        {
+            [TestAspect]
+            public int Number { get; set; }
+        }
 
-//        private class Target : TargetBase<Asset1>
-//        {
-//            [TestAspect]
-//            public double Value { get; set; }
-//        }
+        private class TargetBase<T> : SuperTargetBase<T>
+        {
+            [TestAspect]
+            public string Text { get; set; }
+        }
 
-//        [Aspect(Scope.PerInstance)]
-//        [Injection(typeof(TestAspect))]
-//        private class TestAspect : Attribute
-//        {
-//            [Advice(Kind.Before)]
-//            public void Before()
-//            {
-//                Console.WriteLine("Before");
-//            }
-//        }
-//    }
-//}
+        private class Target : TargetBase<Asset1>
+        {
+            [TestAspect]
+            public double Value { get; set; }
+
+            void Control()
+            {
+                var a = base.Text;
+                var b = base.Number;
+            }
+        }        
+    }
+}
---
+++ tests/AspectInjector.Tests.RuntimeAssets/CrossAssemblyHelpers/SomeType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspectInjector.Tests.RuntimeAssets.CrossAssemblyHelpers
+{
+    public class SomeType
+    {
+    }
+}
