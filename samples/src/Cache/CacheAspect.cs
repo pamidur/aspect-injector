ac4a4d3195362c936f5e1bd939a17c58b619c031
Oleksandr Hulyi <ohulyi@outlook.com>
2020-02-10 17:02:05 +0100

    added Cache aspect sample
    Signed-off-by: Oleksandr Hulyi <ohulyi@outlook.com>

---
+++ samples/src/Cache/CacheAspect.cs
@@ -0,0 +1,81 @@
+﻿using AspectInjector.Broker;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Caching;
+using System.Threading.Tasks;
+
+namespace Aspects.Cache
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    [Injection(typeof(CacheAspect), Inherited = true)]
+    public abstract class CacheAttribute : Attribute
+    {
+        public abstract ObjectCache Cache { get; }
+
+        public abstract CacheItemPolicy Policy { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class MemoryCacheAttribute : CacheAttribute
+    {
+        private static readonly MemoryCache _cache = new MemoryCache("aspect_builtin_memory_cache");
+        private readonly uint _seconds;
+
+        public MemoryCacheAttribute(uint seconds)
+        {
+            _seconds = seconds;
+        }
+
+        public override ObjectCache Cache => _cache;
+        public override CacheItemPolicy Policy => new CacheItemPolicy() { AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(_seconds) };
+    }
+
+
+    [Aspect(Scope.Global)]
+    public class CacheAspect
+    {
+        private static readonly Type _voidTaskResult = Task.FromException(new Exception()).GetType();
+
+        [Advice(Kind.Around)]
+        public object Handle(
+            [Argument(Source.Target)] Func<object[], object> target,
+            [Argument(Source.Arguments)] object[] args,
+            [Argument(Source.Name)] string name,
+            [Argument(Source.ReturnType)] Type retType,
+            [Argument(Source.Triggers)] Attribute[] triggers
+            )
+        {
+            object result = null;
+            var resultFound = false;
+
+            var cacheTriggers = triggers.OfType<CacheAttribute>();
+            var key = GetKey(target.Method, args);
+
+            foreach (var cache in cacheTriggers.Select(ct => ct.Cache).Distinct())
+            {
+                var ci = cache.GetCacheItem(key);
+                if (ci != null)
+                {
+                    result = ci.Value;
+                    resultFound = true;
+                    break;
+                }
+            }
+
+            if (!resultFound)
+            {
+                result = target(args);
+
+                foreach (var cache in cacheTriggers)
+                    cache.Cache.Set(key, result, cache.Policy);
+            }
+
+            return result;
+        }
+
+        private string GetKey(MethodInfo method, object[] args) => $"{method.ToString()}{args.Select(a => a.GetHashCode()).Sum()}";
+
+        private bool IsVoid(Type type) => type == typeof(void) || type == typeof(Task) || type == _voidTaskResult;
+    }
+}
---
+++ samples/tests/Aspests.Tests/CacheTests.cs
@@ -0,0 +1,33 @@
+﻿using Aspects.Cache;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Aspests.Tests
+{
+    public class CacheTests
+    {
+        class TestCalss
+        {
+            [MemoryCache(10)]
+            public long Calculate(int a, string b)
+            {
+                return a + b.GetHashCode() + DateTime.Now.Ticks;
+            }
+        }
+
+        [Fact]
+        public async Task Cache_Aspect_Caches_Method_Result()
+        {
+            var target = new TestCalss();
+            var expected = target.Calculate(10, "test");
+            await Task.Delay(10);
+            var result = target.Calculate(10, "test");
+            await Task.Delay(10);
+            var result2 = target.Calculate(101, "test1");
+
+            Assert.Equal(expected, result);
+            Assert.NotEqual(result2, result);
+        }
+    }
+}
